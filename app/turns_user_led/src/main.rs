89867e201720bb2df45947fe3ed0038c5263f382
silent-rain <2367221387@qq.com>
2023-08-12 13:21:43 +0800

    完善点灯的示例

--- app/turns_user_led/src/main.rs
+++ app/turns_user_led/src/main.rs
@@ -1,40 +1,46 @@
-//! Turns the user LED on
-//!
-//! If compiled for the stm32f103, this assumes that an active low LED is connected to pc13 as
-//! is the case on the blue pill board.
-//!
-//! If compiled for the stm32f100, this assumes that an active high LED is connected to pc9
-//!
-//! Note: Without additional hardware, PC13 should not be used to drive a LED, see
-//! section 5.1.2 of the reference manual for an explanation.
-//! This is not an issue on the blue pill.
+//! 点灯
 
+// 允许有空的循环结构；
 #![allow(clippy::empty_loop)]
+// 禁止使用 Rust 的 unsafe 代码；
 #![deny(unsafe_code)]
-#![no_main]
+// 不使用 Rust 标准库；
 #![no_std]
+// 标记该程序没有使用标准的 main 函数作为程序入口；
+#![no_main]
 
+// 用于处理错误情况；
 use panic_halt as _;
 
+// 用于标记程序入口；
 use cortex_m_rt::entry;
+// 可用于输出和相关AlternateMode引脚的回转速率
+use stm32f1xx_hal::gpio::IOPinSpeed;
+// 用于设置IO引脚的转换速率; 最初，所有引脚都设置为最大转换速率
+use stm32f1xx_hal::gpio::OutputSpeed;
+// 微控制器的外围访问API
 use stm32f1xx_hal::pac;
+// 在独立引脚和寄存器中拆分GPIO外设的扩展特性
 use stm32f1xx_hal::prelude::_stm32_hal_gpio_GpioExt;
 
+// 标记接下来的函数是程序的入口点；
 #[entry]
 fn main() -> ! {
+    // 获取对特定设备外设的访问权限
     let p = pac::Peripherals::take().unwrap();
 
-    #[allow(unused)]
-    let mut gpioc = p.GPIOC.split();
+    // 通过拆分 GPIOA 区块，获取对其各引脚的互斥访问。
+    let mut gpioa = p.GPIOA.split();
 
-    #[cfg(feature = "stm32f100")]
-    gpioc.pc9.into_push_pull_output(&mut gpioc.crh).set_high();
+    // 将 PA0 引脚配置为推挽式输出。
+    let mut led = gpioa.pa0.into_push_pull_output(&mut gpioa.crl);
+    // 设置其输出速度（50 MHz）。
+    // 然后在接下来的代码中，我们将使用该引脚来控制 LED 的状态。
+    led.set_speed(&mut gpioa.crl, IOPinSpeed::Mhz50);
 
-    #[cfg(feature = "stm32f101")]
-    gpioc.pc9.into_push_pull_output(&mut gpioc.crh).set_high();
+    // 设置低电平
+    led.set_low();
 
-    #[cfg(any(feature = "stm32f103", feature = "stm32f105", feature = "stm32f107"))]
-    gpioc.pc13.into_push_pull_output(&mut gpioc.crh).set_low();
-
+    // 无限循环
     loop {}
 }
