25d2bb7276f971204ab99a5c49c7be15256ac17d
silent-rain <2367221387@qq.com>
2023-08-12 12:48:57 +0800

    完善 Hello World 示例

---
+++ app/helloworld/README.md
@@ -0,0 +1,23 @@
+# Hello World
+
+这个是一个简单的 Hello World 示例。
+
+## 学习目标
+
+- 了解项目结构
+- 项目配置了解
+  - 了解项目编译目标的配置 `.cargo/config.toml`
+  - 了解 vscode 的配置, 用于消除编译器告警 `.vscode/settings.json`
+  - 了解工作空间配置 `Cargo.toml`
+  - 了解 GDB 调试的自动接入脚本 `.gdbinit`
+  - 了解 Renode 仿真模拟器的配置 `scripts/stm32f103.resc`
+- 编译
+  - debug 编译
+  - release 编译
+- Renode 仿真模拟器的使用
+  - Renode CLI 启动
+  - stm32 仿真器启动
+- GDB 调试
+  - GDB 手动启动
+  - GDB 自动化脚本启动
+  - 调试与打印代码中的变量
---
+++ app/helloworld/src/main.rs
@@ -0,0 +1,44 @@
+//! Prints "Hello, world" on the OpenOCD console
+
+// 允许有空的循环结构；
+#![allow(clippy::empty_loop)]
+// 禁止使用 Rust 的 unsafe 代码；
+#![deny(unsafe_code)]
+// 标记该程序没有使用标准的 main 函数作为程序入口；
+#![no_main]
+// 不使用 Rust 标准库；
+#![no_std]
+
+// 用于处理错误情况；
+use panic_semihosting as _;
+
+// 便于程序调试；
+use cortex_m_semihosting::debug;
+// 用于在 OpenOCD 终端上输出信息；
+use cortex_m_semihosting::hprintln;
+// 用于标记程序入口；
+use cortex_m_rt::entry;
+
+// 标记接下来的函数是程序的入口点；
+// fn main() -> ! 定义程序的入口函数 main，返回类型为 `!`，表示这个函数不应该返回；
+#[entry]
+fn main() -> ! {
+    // _y 的可变变量，但未初始化；
+    let mut _y;
+    // 定义变量 x，并将其值设置为 42；
+    let x = 42;
+    // 将变量 x 的值赋给 _y；
+    _y = x;
+    // 通过 OpenOCD 终端输出 `_y` 变量的值，使用十六进制表示；
+    hprintln!("y: {:#?}", _y);
+    // 通过 OpenOCD 终端输出 "Hello, world!"；
+    hprintln!("Hello, world!");
+
+    // 退出 Renode
+    // 执行到此处时通知 OpenOCD 结束程序，并返回正常退出状态。
+    // 注意不要在实际硬件上运行这一行代码，它会导致 OpenOCD 失败；
+    debug::exit(debug::EXIT_SUCCESS);
+
+    // 进入一个空的无限循环，防止程序异常退出。
+    loop {}
+}
