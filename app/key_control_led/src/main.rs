28daf50295939182503a6e5afc53c62ba3139a5e
silent-rain <2367221387@qq.com>
2023-08-13 23:14:16 +0800

    程序封装

--- app/key_control_led/src/main.rs
+++ app/key_control_led/src/main.rs
@@ -6,58 +6,29 @@
 
 use cortex_m::prelude::_embedded_hal_blocking_delay_DelayMs;
 use cortex_m_rt::entry;
-use stm32f1xx_hal::gpio;
-use stm32f1xx_hal::gpio::IOPinSpeed;
-use stm32f1xx_hal::gpio::OutputSpeed;
-use stm32f1xx_hal::gpio::PinState;
-use stm32f1xx_hal::gpio::PullUp;
+use stm32f1xx_hal::flash::{self, FlashExt};
+use stm32f1xx_hal::gpio::{self, gpioa, gpiob, GpioExt, OutputSpeed};
 use stm32f1xx_hal::pac;
-use stm32f1xx_hal::prelude::_stm32_hal_flash_FlashExt;
-use stm32f1xx_hal::prelude::_stm32_hal_gpio_GpioExt;
-use stm32f1xx_hal::rcc::RccExt;
-use stm32f1xx_hal::timer::SysDelay;
-use stm32f1xx_hal::timer::SysTimerExt;
+use stm32f1xx_hal::rcc::{self, RccExt};
+use stm32f1xx_hal::timer::{SysDelay, SysTimerExt};
 
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
     // 获取对外设的访问对象
-    let cp: cortex_m::Peripherals = cortex_m::Peripherals::take().unwrap();
-    let dp: pac::Peripherals = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
 
-    // 获得原始flash和rcc设备的所有权，并将它们转换为相应的HAL结构
-    let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC.constrain();
-
-    // 冻结系统中所有时钟的配置，并将冻结的频率存储在时钟中
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    // 具有自定义精度的阻塞延迟
-    let mut delay = cp.SYST.delay(&clocks);
-
-    // 获取GPIO外围设备
-    let mut gpioa = dp.GPIOA.split();
+    // 封装具有自定义精度的阻塞延迟函数
+    let mut delay = sys_delay(cp, dp.FLASH.constrain(), dp.RCC.constrain());
 
     // LED
-    // 将 pin 引脚配置为推挽式输出。
-    let mut led1 = gpioa
-        .pa1
-        .into_push_pull_output_with_state(&mut gpioa.crl, PinState::High);
-    let mut led2 = gpioa
-        .pa2
-        .into_push_pull_output_with_state(&mut gpioa.crl, PinState::High);
-    // 设置其输出速度（50 MHz）。
-    led1.set_speed(&mut gpioa.crl, IOPinSpeed::Mhz50);
-    led2.set_speed(&mut gpioa.crl, IOPinSpeed::Mhz50);
-
-    let mut gpiob = dp.GPIOB.split();
-
+    let gpioa = dp.GPIOA.split();
+    let (mut led1, mut led2) = init_led(gpioa);
     // 按键
-    // 将引脚配置为作为上拉输入引脚操作
-    // 输入模式中速度是没有用的, 无需配置
-    let mut key1 = gpiob.pb1.into_pull_up_input(&mut gpiob.crl);
-    let mut key11 = gpiob.pb11.into_pull_up_input(&mut gpiob.crh);
+    let gpiob: gpio::gpiob::Parts = dp.GPIOB.split();
+    let (mut key1, mut key11) = init_key(gpiob);
 
     // 等待计时器触发更新并更改LED的状态
     loop {
@@ -73,10 +44,54 @@ fn main() -> ! {
     }
 }
 
+/// 封装具有自定义精度的阻塞延迟函数
+fn sys_delay(cp: cortex_m::Peripherals, mut flash: flash::Parts, rcc: rcc::Rcc) -> SysDelay {
+    // 冻结系统中所有时钟的配置，并将冻结的频率存储在时钟中
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    // 具有自定义精度的阻塞延迟
+    let delay: SysDelay = cp.SYST.delay(&clocks);
+    delay
+}
+
+/// LED
+/// 将 pin 引脚配置为推挽式输出
+fn init_led(
+    mut gpioa: gpioa::Parts,
+) -> (
+    gpio::PA1<gpio::Output<gpio::PushPull>>,
+    gpio::PA2<gpio::Output<gpio::PushPull>>,
+) {
+    let mut led1 = gpioa
+        .pa1
+        .into_push_pull_output_with_state(&mut gpioa.crl, gpio::PinState::High);
+    let mut led2 = gpioa
+        .pa2
+        .into_push_pull_output_with_state(&mut gpioa.crl, gpio::PinState::High);
+    // 设置其输出速度（50 MHz）。
+    led1.set_speed(&mut gpioa.crl, gpio::IOPinSpeed::Mhz50);
+    led2.set_speed(&mut gpioa.crl, gpio::IOPinSpeed::Mhz50);
+    (led1, led2)
+}
+
+/// 按键
+/// 将引脚配置为作为上拉输入引脚操作
+/// 输入模式中速度是没有用的, 无需配置
+fn init_key(
+    mut gpiob: gpiob::Parts,
+) -> (
+    gpio::PB1<gpio::Input<gpio::PullUp>>,
+    gpio::PB11<gpio::Input<gpio::PullUp>>,
+) {
+    let key1 = gpiob.pb1.into_pull_up_input(&mut gpiob.crl);
+    let key11 = gpiob.pb11.into_pull_up_input(&mut gpiob.crh);
+    (key1, key11)
+}
+
 /// 获取按键的值
 fn get_key_num(
-    key1: &mut gpio::Pin<'B', 1, gpio::Input<PullUp>>,
-    key11: &mut gpio::Pin<'B', 11, gpio::Input<PullUp>>,
+    key1: &mut gpio::Pin<'B', 1, gpio::Input<gpio::PullUp>>,
+    key11: &mut gpio::Pin<'B', 11, gpio::Input<gpio::PullUp>>,
     delay: &mut SysDelay,
 ) -> i32 {
     let mut key_num = 0;
