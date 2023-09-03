69c53abd2ca19634356d22b8301166f2a75409d0
silent-rain <2367221387@qq.com>
2023-09-04 00:22:26 +0800

    代码优化

--- core/stm32f10x-rs/stm32f10x/System/delay.c
+++ core/stm32f10x-rs/stm32f10x/System/delay.c
@@ -8,7 +8,8 @@
   */
 void Delay_us(uint32_t xus)
 {
-	SysTick->LOAD = 72 * xus;				//设置定时器重装值
+	// SysTick->LOAD = 72 * xus;			//设置定时器重装值
+	SysTick->LOAD = xus;					//设置定时器重装值, 转换rust后的延时
 	SysTick->VAL = 0x00;					//清空当前计数值
 	SysTick->CTRL = 0x00000005;				//设置时钟源为HCLK，启动定时器
 	while(!(SysTick->CTRL & 0x00010000));	//等待计数到0
