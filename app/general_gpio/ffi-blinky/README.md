8d63a889a2ae523fee5f4eb8f033f62a05d665a7
silent-rain <2367221387@qq.com>
2023-09-03 21:01:27 +0800

    代码优化

--- app/general_gpio/ffi-blinky/README.md
+++ app/general_gpio/ffi-blinky/README.md
@@ -17,3 +17,7 @@ cargo run --target thumbv7m-none-eabi -p ffi-blinky probe-run -- --chip STM32F10
 - 设置定时器
 - 设置高电平
 - 设置低电平
+
+## 接线图
+
+![](../../images/../../images/3-1%20LED闪烁.jpg)
--- app/general_gpio/ffi-blinky/src/main.rs
+++ app/general_gpio/ffi-blinky/src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+use core::ffi::c_uint;
+
+use defmt::println;
 use defmt_rtt as _;
 use panic_probe as _;
 
@@ -8,8 +11,9 @@ use cortex_m_rt::entry;
 use stm32f1xx_hal as _;
 
 use stm32f10x_rs::{
-    Delay_ms, FunctionalState, GPIOMode_TypeDef, GPIOSpeed_TypeDef, GPIO_Init, GPIO_InitTypeDef,
-    GPIO_ResetBits, GPIO_SetBits, GPIO_TypeDef, RCC_APB2PeriphClockCmd,
+    BitAction, Delay_ms, FunctionalState, GPIOMode_TypeDef, GPIOSpeed_TypeDef, GPIO_Init,
+    GPIO_InitTypeDef, GPIO_ResetBits, GPIO_SetBits, GPIO_TypeDef, GPIO_WriteBit,
+    RCC_APB2PeriphClockCmd,
 };
 
 // #define GPIO_Pin_0                 ((uint16_t)0x0001)  /*!< Pin 0 selected */
@@ -20,32 +24,45 @@ const GPIO_Pin_0: u16 = 0x0001;
 #[allow(non_upper_case_globals)]
 const RCC_APB2Periph_GPIOA: u32 = 0x00000004;
 
-// #define GPIOA               ((GPIO_TypeDef *) GPIOA_BASE)
-// #define GPIOA_BASE            (APB2PERIPH_BASE + 0x0800)
-// #define APB2PERIPH_BASE       (PERIPH_BASE + 0x10000)
 // #define PERIPH_BASE           ((uint32_t)0x40000000) /*!< Peripheral base address in the alias region */
-#[allow(non_upper_case_globals)]
-const GPIOA_BASE: u32 = 0x40000000 + 0x10000 + 0x0800;
+// #define APB2PERIPH_BASE       (PERIPH_BASE + 0x10000)
+// #define GPIOA_BASE            (APB2PERIPH_BASE + 0x0800)
+// #define GPIOA               ((GPIO_TypeDef *) GPIOA_BASE)
+const PERIPH_BASE: c_uint = 0x40000000;
+const APB2PERIPH_BASE: *mut c_uint = (PERIPH_BASE + 0x10000) as *mut c_uint;
+const GPIOA_BASE: *mut u32 = APB2PERIPH_BASE.wrapping_add(0x0800);
+const GPIOA: *mut GPIO_TypeDef = GPIOA_BASE as *mut GPIO_TypeDef;
 
 #[entry]
 fn main() -> ! {
-    unsafe { RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, FunctionalState::ENABLE) };
+    unsafe {
+        println!("GPIOA_BASE {:?}", GPIOA_BASE);
+        println!("GPIOA {:?}", &GPIOA as *const _ as u32);
 
-    #[allow(non_snake_case)]
-    let mut GPIOA = unsafe { *(GPIOA_BASE as *const GPIO_TypeDef) };
+        println!("RCC_APB2PeriphClockCmd");
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, FunctionalState::ENABLE);
 
-    let mut gpio_init_structure = GPIO_InitTypeDef {
-        GPIO_Mode: GPIOMode_TypeDef::GPIO_Mode_Out_PP,
-        GPIO_Pin: GPIO_Pin_0,
-        GPIO_Speed: GPIOSpeed_TypeDef::GPIO_Speed_50MHz,
-    };
-    unsafe { GPIO_Init(&mut GPIOA as *mut GPIO_TypeDef, &mut gpio_init_structure) };
-    loop {
-        unsafe {
-            GPIO_ResetBits(&mut GPIOA as *mut GPIO_TypeDef, GPIO_Pin_0);
-            Delay_ms(500);
-            GPIO_SetBits(&mut GPIOA as *mut GPIO_TypeDef, GPIO_Pin_0);
-            Delay_ms(500);
+        println!("GPIO_Init");
+        let mut gpio_init_structure = GPIO_InitTypeDef {
+            GPIO_Mode: GPIOMode_TypeDef::GPIO_Mode_Out_PP,
+            GPIO_Pin: GPIO_Pin_0,
+            GPIO_Speed: GPIOSpeed_TypeDef::GPIO_Speed_50MHz,
         };
+        GPIO_Init(GPIOA, &mut gpio_init_structure);
+
+        println!("loop");
+        loop {
+            println!("d...");
+            GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+            Delay_ms(500);
+            GPIO_SetBits(GPIOA, GPIO_Pin_0);
+            Delay_ms(500);
+
+            println!("dd...");
+            GPIO_WriteBit(GPIOA, GPIO_Pin_0, BitAction::Bit_RESET);
+            Delay_ms(500);
+            GPIO_WriteBit(GPIOA, GPIO_Pin_0, BitAction::Bit_SET);
+            Delay_ms(500);
+        }
     }
 }
