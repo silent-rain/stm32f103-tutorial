dd6d0ee7552055ee09d06d39f1ccc1a8424ae940
silent-rain <2367221387@qq.com>
2023-09-12 21:56:07 +0800

    代码优化

--- core/hardware/src/mpu6050/conf.rs
+++ core/hardware/src/mpu6050/conf.rs
@@ -1,19 +1,28 @@
 //!配置
 pub const DEFAULT_SLAVE_ADDR: u8 = 0x68;
 
+// 采样率分频，典型值：0x07(125Hz)
 pub const MPU6050_SMPLRT_DIV: u8 = 0x19;
+// 低通滤波频率，典型值：0x06(5Hz)
 pub const MPU6050_CONFIG: u8 = 0x1A;
+// 陀螺仪自检及测量范围，典型值：0x18(不自检，2000deg/s)
 pub const MPU6050_GYRO_CONFIG: u8 = 0x1B;
+// 加速计自检、测量范围及高通滤波频率，典型值：0x01(不自检，2G，5Hz)
 pub const MPU6050_ACCEL_CONFIG: u8 = 0x1C;
 
+// 存储最近的X轴、Y轴、Z轴加速度感应器的测量值
 pub const MPU6050_ACCEL_XOUT_H: u8 = 0x3B;
 pub const MPU6050_ACCEL_XOUT_L: u8 = 0x3C;
 pub const MPU6050_ACCEL_YOUT_H: u8 = 0x3D;
 pub const MPU6050_ACCEL_YOUT_L: u8 = 0x3E;
 pub const MPU6050_ACCEL_ZOUT_H: u8 = 0x3F;
 pub const MPU6050_ACCEL_ZOUT_L: u8 = 0x40;
+
+// 存储的最近温度传感器的测量值
 pub const MPU6050_TEMP_OUT_H: u8 = 0x41;
 pub const MPU6050_TEMP_OUT_L: u8 = 0x42;
+
+// 存储最近的X轴、Y轴、Z轴陀螺仪感应器的测量值
 pub const MPU6050_GYRO_XOUT_H: u8 = 0x43;
 pub const MPU6050_GYRO_XOUT_L: u8 = 0x44;
 pub const MPU6050_GYRO_YOUT_H: u8 = 0x45;
@@ -21,6 +30,8 @@ pub const MPU6050_GYRO_YOUT_L: u8 = 0x46;
 pub const MPU6050_GYRO_ZOUT_H: u8 = 0x47;
 pub const MPU6050_GYRO_ZOUT_L: u8 = 0x48;
 
+// 电源管理，典型值：0x00(正常启用)
 pub const MPU6050_PWR_MGMT_1: u8 = 0x6B;
 pub const MPU6050_PWR_MGMT_2: u8 = 0x6C;
+// IIC地址寄存器(默认数值0x68，只读)
 pub const MPU6050_WHO_AM_I: u8 = 0x75;
--- core/hardware/src/mpu6050/mpu6050_reg.rs
+++ core/hardware/src/mpu6050/mpu6050_reg.rs
@@ -4,231 +4,241 @@
 use super::conf::*;
 pub use super::AccelGyroData;
 
-use embedded_hal::digital::v2::{OutputPin, StatefulOutputPin};
-use stm32f1xx_hal::gpio::{self, OutputSpeed};
+use embedded_hal::{
+    digital::v2::{OutputPin, StatefulOutputPin},
+    prelude::_embedded_hal_blocking_delay_DelayUs,
+};
+use stm32f1xx_hal::{
+    gpio::{self, OutputSpeed},
+    timer::SysDelay,
+};
 
-pub fn i2c_r_sda<Sda>(sda: &mut Sda) -> bool
+/// MPU6050 芯片
+pub struct Mpu6050<'a, Scl, Sda>
 where
+    Scl: OutputPin,
     Sda: OutputPin + StatefulOutputPin,
     <Sda as OutputPin>::Error: core::fmt::Debug,
 {
-    sda.is_set_high().unwrap()
+    scl: &'a mut Scl,
+    sda: &'a mut Sda,
+    delay: &'a mut SysDelay,
 }
 
-/// I2C 开始
-pub fn i2c_start<Scl, Sda>(scl: &mut Scl, sda: &mut Sda)
+impl<'a, Scl, Sda> Mpu6050<'a, Scl, Sda>
 where
     Scl: OutputPin,
-    Sda: OutputPin,
+    <Scl as OutputPin>::Error: core::fmt::Debug,
+    Sda: OutputPin + StatefulOutputPin,
+    <Sda as OutputPin>::Error: core::fmt::Debug,
 {
-    let _ = sda.set_high();
-    let _ = scl.set_high();
-    let _ = sda.set_low();
-    let _ = scl.set_low();
-}
+    pub fn new(scl: &'a mut Scl, sda: &'a mut Sda, delay: &'a mut SysDelay) -> Self {
+        Mpu6050 { scl, sda, delay }
+    }
 
-/// I2C 结束
-pub fn i2c_stop<Scl, Sda>(scl: &mut Scl, sda: &mut Sda)
-where
-    Scl: OutputPin,
-    Sda: OutputPin,
-{
-    let _ = sda.set_low();
-    let _ = scl.set_high();
-    let _ = sda.set_high();
-}
-
-/// I2C 发送字节
-pub fn i2c_send_byte<Scl, Sda>(scl: &mut Scl, sda: &mut Sda, byte: u8)
-where
-    Scl: OutputPin,
-    Sda: OutputPin,
-{
-    for i in 0..8 {
-        if byte & (0x80 >> i) > 0 {
-            let _ = sda.set_high();
+    fn i2c_w_scl(&mut self, bit_value: u8) {
+        if bit_value > 0 {
+            self.scl.set_high();
         } else {
-            let _ = sda.set_low();
+            self.scl.set_low();
         }
-        let _ = scl.set_high();
-        let _ = scl.set_low();
+        self.delay.delay_us(10_u32);
+    }
+
+    fn i2c_w_sda(&mut self, bit_value: u8) {
+        if bit_value > 0 {
+            self.sda.set_high();
+        } else {
+            self.sda.set_low();
+        }
+        self.delay.delay_us(10_u32);
+    }
+
+    fn i2c_r_sda(&mut self) -> u8 {
+        let bit_value = self.sda.is_set_high().unwrap();
+        self.delay.delay_us(10_u32);
+        if bit_value {
+            1
+        } else {
+            0
+        }
+    }
+
+    /// 产生 I2C 协议起始信号
+    fn i2c_start(&mut self) {
+        self.i2c_w_sda(1);
+        self.i2c_w_scl(1);
+        self.i2c_w_sda(0);
+        self.i2c_w_scl(0);
+    }
+
+    /// 产生 I2C 协议结束信号
+    pub fn i2c_stop(&mut self) {
+        self.i2c_w_sda(0);
+        self.i2c_w_scl(1);
+        self.i2c_w_sda(1);
+    }
+
+    /// 发送八位数据（不包含应答）
+    pub fn i2c_send_byte(&mut self, byte: u8) {
+        for i in 0..8 {
+            self.i2c_w_sda(byte & (0x80 >> i));
+            self.i2c_w_scl(1);
+            self.i2c_w_scl(0);
+        }
+    }
+
+    /// 读取八位数据（不包含应答）
+    pub fn i2c_receive_byte(&mut self) -> u8 {
+        let mut byte = 0x00;
+        self.i2c_w_sda(1);
+        for i in 0..8 {
+            self.i2c_w_scl(1);
+            if self.i2c_r_sda() == 1 {
+                byte |= (0x80 >> i);
+            }
+            self.i2c_w_scl(0);
+        }
+        byte
+    }
+
+    /// 发送应答信号
+    pub fn i2c_send_ack(&mut self, ack_bit: u8) {
+        self.i2c_w_sda(ack_bit);
+        self.i2c_w_scl(1);
+    }
+
+    /// 接收应答信号
+    pub fn i2c_receive_ack(&mut self) -> u8 {
+        self.i2c_w_sda(1);
+        self.i2c_w_scl(1);
+        let ack_bit = self.i2c_r_sda();
+        self.i2c_w_scl(0);
+        ack_bit
+    }
+
+    /// I2C 初始化
+    /// open-drain output pin 10,11
+    pub fn init_i2c(&mut self) {
+        self.i2c_w_scl(1);
+        self.i2c_w_sda(1);
     }
 }
 
-/// I2C 接收字节
-pub fn i2c_receive_byte<Scl, Sda>(scl: &mut Scl, sda: &mut Sda) -> u8
-where
-    Scl: OutputPin + StatefulOutputPin,
-    <Scl as OutputPin>::Error: core::fmt::Debug,
-    Sda: OutputPin + StatefulOutputPin,
-    <Sda as OutputPin>::Error: core::fmt::Debug,
-{
-    let mut byte: u8 = 0x00;
-    let _ = sda.set_high();
-
-    for i in 0..8 {
-        let _ = scl.set_high();
-
-        if i2c_r_sda(sda) {
-            byte |= 0x80 >> i;
-        }
-
-        let _ = scl.set_low();
-    }
-    byte
-}
-
-/// I2C 发送 ACK
-pub fn i2c_send_ack<Scl, Sda>(scl: &mut Scl, sda: &mut Sda, ack_bit: u8)
+impl<'a, Scl, Sda> Mpu6050<'a, Scl, Sda>
 where
     Scl: OutputPin,
-    Sda: OutputPin,
+    <Scl as OutputPin>::Error: core::fmt::Debug,
+    Sda: OutputPin + StatefulOutputPin,
+    <Sda as OutputPin>::Error: core::fmt::Debug,
 {
-    if ack_bit == 0 {
-        let _ = sda.set_low();
-    } else {
-        let _ = sda.set_high();
+    /// MPU6050 写寄存器函数
+    /// reg_address：寄存器地址
+    /// data：待写入寄存器值
+    pub fn write_reg(&mut self, reg_address: u8, data: u8) {
+        // 发送起始信号
+        self.i2c_start();
+        // 发送设备地址
+        self.i2c_send_byte(DEFAULT_SLAVE_ADDR);
+        self.i2c_receive_ack();
+
+        // 发送寄存器地址
+        self.i2c_send_byte(reg_address);
+        self.i2c_receive_ack();
+
+        // 写数据到寄存器
+        self.i2c_send_byte(data);
+        self.i2c_receive_ack();
+        self.i2c_stop();
     }
-    let _ = scl.set_high();
-    let _ = scl.set_low();
-}
 
-/// I2C 接收 ACK
-pub fn i2c_receive_ack<Scl, Sda>(scl: &mut Scl, sda: &mut Sda) -> bool
-where
-    Scl: OutputPin + StatefulOutputPin,
-    <Scl as OutputPin>::Error: core::fmt::Debug,
-    Sda: OutputPin + StatefulOutputPin,
-    <Sda as OutputPin>::Error: core::fmt::Debug,
-{
-    let _ = sda.set_high();
-    let _ = scl.set_high();
-    let ack_bit = i2c_r_sda(sda);
-    let _ = scl.set_low();
-    ack_bit
-}
+    /// 读取寄存器
+    pub fn read_reg(&mut self, reg_address: u8) -> i16 {
+        // 发送起始信号
+        self.i2c_start();
 
-/// I2C 初始化
-/// open-drain output pin 10,11
-pub fn init_i2c<Scl, Sda>(scl: &mut Scl, sda: &mut Sda)
-where
-    Scl: OutputPin,
-    Sda: OutputPin,
-{
-    let _ = scl.set_high();
-    let _ = sda.set_high();
-}
+        // 发送设备地址
+        self.i2c_send_byte(DEFAULT_SLAVE_ADDR);
+        self.i2c_receive_ack();
 
-/// 写入寄存器
-pub fn write_reg<Scl, Sda>(scl: &mut Scl, sda: &mut Sda, reg_address: u8, data: u8)
-where
-    Scl: OutputPin + StatefulOutputPin,
-    <Scl as OutputPin>::Error: core::fmt::Debug,
-    Sda: OutputPin + StatefulOutputPin,
-    <Sda as OutputPin>::Error: core::fmt::Debug,
-{
-    i2c_start(scl, sda);
-    i2c_send_byte(scl, sda, DEFAULT_SLAVE_ADDR);
-    i2c_receive_ack(scl, sda);
-    i2c_send_byte(scl, sda, reg_address);
-    i2c_receive_ack(scl, sda);
-    i2c_send_byte(scl, sda, data);
-    i2c_receive_ack(scl, sda);
-    i2c_stop(scl, sda);
-}
+        // 发送寄存器地址
+        self.i2c_send_byte(reg_address);
+        self.i2c_receive_ack();
 
-/// 读取寄存器
-pub fn read_reg<Scl, Sda>(scl: &mut Scl, sda: &mut Sda, reg_address: u8) -> i16
-where
-    Scl: OutputPin + StatefulOutputPin,
-    <Scl as OutputPin>::Error: core::fmt::Debug,
-    Sda: OutputPin + StatefulOutputPin,
-    <Sda as OutputPin>::Error: core::fmt::Debug,
-{
-    i2c_start(scl, sda);
-    i2c_send_byte(scl, sda, DEFAULT_SLAVE_ADDR);
-    i2c_receive_ack(scl, sda);
-    i2c_send_byte(scl, sda, reg_address);
-    i2c_receive_ack(scl, sda);
+        // 发送重复起始信号
+        self.i2c_start();
+        // 发送读模式设备地址
+        self.i2c_send_byte(DEFAULT_SLAVE_ADDR | 0x01);
+        self.i2c_receive_ack();
 
-    i2c_start(scl, sda);
-    i2c_send_byte(scl, sda, DEFAULT_SLAVE_ADDR | 0x01);
-    i2c_receive_ack(scl, sda);
-    let data = i2c_receive_byte(scl, sda);
-    i2c_send_ack(scl, sda, 1);
-    i2c_stop(scl, sda);
+        // 读寄存器数据
+        let data = self.i2c_receive_byte();
+        // 非应答信号
+        self.i2c_send_ack(1);
+        self.i2c_stop();
 
-    data as i16
-}
+        data as i16
+    }
 
-/// MPU6050 初始化
-/// ```rust
-/// let mut scl = gpiob.pb10.into_open_drain_output(&mut gpiob.crh);
-/// let mut sda = gpiob.pb11.into_open_drain_output(&mut gpiob.crh);
-/// sda.set_speed(&mut gpiob.crh, gpio::IOPinSpeed::Mhz50);
-/// scl.set_speed(&mut gpiob.crh, gpio::IOPinSpeed::Mhz50);
-/// hardware::mpu6050::init_mpu6050(&mut scl, &mut sda);
-/// ```
-pub fn init_mpu6050<Scl, Sda>(scl: &mut Scl, sda: &mut Sda)
-where
-    Scl: OutputPin + StatefulOutputPin,
-    <Scl as OutputPin>::Error: core::fmt::Debug,
-    Sda: OutputPin + StatefulOutputPin,
-    <Sda as OutputPin>::Error: core::fmt::Debug,
-{
-    init_i2c(scl, sda);
-    write_reg(scl, sda, MPU6050_PWR_MGMT_1, 0x01);
-    write_reg(scl, sda, MPU6050_PWR_MGMT_2, 0x00);
-    write_reg(scl, sda, MPU6050_SMPLRT_DIV, 0x09);
-    write_reg(scl, sda, MPU6050_CONFIG, 0x06);
-    write_reg(scl, sda, MPU6050_GYRO_CONFIG, 0x18);
-    write_reg(scl, sda, MPU6050_ACCEL_CONFIG, 0x18);
-}
+    /// MPU6050 初始化
+    /// ```rust
+    /// let mut scl = gpiob.pb10.into_open_drain_output(&mut gpiob.crh);
+    /// let mut sda = gpiob.pb11.into_open_drain_output(&mut gpiob.crh);
+    /// sda.set_speed(&mut gpiob.crh, gpio::IOPinSpeed::Mhz50);
+    /// scl.set_speed(&mut gpiob.crh, gpio::IOPinSpeed::Mhz50);
+    /// hardware::mpu6050::init_mpu6050(&mut scl, &mut sda);
+    /// ```
+    pub fn init_mpu6050(&mut self) {
+        // I2C 初始化
+        self.init_i2c();
 
-/// 获取 MPU6050 ID
-pub fn get_id<Scl, Sda>(scl: &mut Scl, sda: &mut Sda) -> i16
-where
-    Scl: OutputPin + StatefulOutputPin,
-    <Scl as OutputPin>::Error: core::fmt::Debug,
-    Sda: OutputPin + StatefulOutputPin,
-    <Sda as OutputPin>::Error: core::fmt::Debug,
-{
-    read_reg(scl, sda, MPU6050_WHO_AM_I)
-}
+        // 解除休眠状态
+        self.write_reg(MPU6050_PWR_MGMT_1, 0x01);
+        self.write_reg(MPU6050_PWR_MGMT_2, 0x00);
+        // 陀螺仪采样率，典型值：0x07(125Hz)
+        self.write_reg(MPU6050_SMPLRT_DIV, 0x09);
+        // 低通滤波频率，典型值：0x06(5Hz)
+        self.write_reg(MPU6050_CONFIG, 0x06);
+        // 陀螺仪自检及测量范围，典型值：0x18(不自检，2000deg/s)
+        self.write_reg(MPU6050_GYRO_CONFIG, 0x18);
+        // 加速计自检、测量范围及高通滤波频率，典型值：0x01(不自检，2G，5Hz)
+        self.write_reg(MPU6050_ACCEL_CONFIG, 0x18);
+    }
 
-/// 获取数据
-pub fn get_data<Scl, Sda>(scl: &mut Scl, sda: &mut Sda) -> AccelGyroData
-where
-    Scl: OutputPin + StatefulOutputPin,
-    <Scl as OutputPin>::Error: core::fmt::Debug,
-    Sda: OutputPin + StatefulOutputPin,
-    <Sda as OutputPin>::Error: core::fmt::Debug,
-{
-    let mut data = AccelGyroData::default();
+    /// 获取 MPU6050 ID
+    pub fn get_id(&mut self) -> i16 {
+        self.read_reg(MPU6050_WHO_AM_I)
+    }
 
-    let data_h = read_reg(scl, sda, MPU6050_ACCEL_XOUT_H);
-    let data_l = read_reg(scl, sda, MPU6050_ACCEL_XOUT_L);
-    data.acc_x = (data_h << 8) | data_l;
+    /// 基本数据读取
+    /// 连续读两个寄存器并合成 16 位数据
+    pub fn get_data(&mut self) -> AccelGyroData {
+        let mut data = AccelGyroData::default();
 
-    let data_h = read_reg(scl, sda, MPU6050_ACCEL_YOUT_H);
-    let data_l = read_reg(scl, sda, MPU6050_ACCEL_YOUT_L);
-    data.acc_y = (data_h << 8) | data_l;
+        let data_h = self.read_reg(MPU6050_ACCEL_XOUT_H);
+        let data_l = self.read_reg(MPU6050_ACCEL_XOUT_L);
+        data.acc_x = (data_h << 8) | data_l;
 
-    let data_h = read_reg(scl, sda, MPU6050_ACCEL_ZOUT_H);
-    let data_l = read_reg(scl, sda, MPU6050_ACCEL_ZOUT_L);
-    data.acc_z = (data_h << 8) | data_l;
+        let data_h = self.read_reg(MPU6050_ACCEL_YOUT_H);
+        let data_l = self.read_reg(MPU6050_ACCEL_YOUT_L);
+        data.acc_y = (data_h << 8) | data_l;
 
-    let data_h = read_reg(scl, sda, MPU6050_GYRO_XOUT_H);
-    let data_l = read_reg(scl, sda, MPU6050_GYRO_XOUT_L);
-    data.gyro_x = (data_h << 8) | data_l;
+        let data_h = self.read_reg(MPU6050_ACCEL_ZOUT_H);
+        let data_l = self.read_reg(MPU6050_ACCEL_ZOUT_L);
+        data.acc_z = (data_h << 8) | data_l;
 
-    let data_h = read_reg(scl, sda, MPU6050_GYRO_YOUT_H);
-    let data_l = read_reg(scl, sda, MPU6050_GYRO_YOUT_L);
-    data.gyro_y = (data_h << 8) | data_l;
+        let data_h = self.read_reg(MPU6050_GYRO_XOUT_H);
+        let data_l = self.read_reg(MPU6050_GYRO_XOUT_L);
+        data.gyro_x = (data_h << 8) | data_l;
 
-    let data_h = read_reg(scl, sda, MPU6050_GYRO_ZOUT_H);
-    let data_l = read_reg(scl, sda, MPU6050_GYRO_ZOUT_L);
-    data.gyro_z = (data_h << 8) | data_l;
+        let data_h = self.read_reg(MPU6050_GYRO_YOUT_H);
+        let data_l = self.read_reg(MPU6050_GYRO_YOUT_L);
+        data.gyro_y = (data_h << 8) | data_l;
 
-    data
+        let data_h = self.read_reg(MPU6050_GYRO_ZOUT_H);
+        let data_l = self.read_reg(MPU6050_GYRO_ZOUT_L);
+        data.gyro_z = (data_h << 8) | data_l;
+
+        data
+    }
 }
