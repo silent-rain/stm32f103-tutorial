9d849327d7516b8e9ad86fca49a761c115e105ba
silent-rain <2367221387@qq.com>
2023-10-09 22:38:58 +0800

    代码示例优化

--- app/delay/syst_delay/src/main.rs
+++ app/delay/syst_delay/src/main.rs
@@ -12,11 +12,12 @@ use stm32f1xx_hal::pac;
 use stm32f1xx_hal::prelude::_fugit_ExtU32;
 use stm32f1xx_hal::prelude::_stm32_hal_flash_FlashExt;
 use stm32f1xx_hal::rcc::RccExt;
-use stm32f1xx_hal::timer::TimerExt;
+use stm32f1xx_hal::timer::SysTimerExt;
 
 #[entry]
 fn main() -> ! {
     // 获取对外设的访问对象
+    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
     // 获得原始flash和rcc设备的所有权，并将它们转换为相应的HAL结构
@@ -26,20 +27,17 @@ fn main() -> ! {
     // 冻结系统中所有时钟的配置，并将冻结的频率存储在时钟中
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    // 基于通用pupose 32位定时器TIM2创建延迟抽象
-
-    //let mut delay = hal::timer::FTimerUs::new(dp.TIM2, &clocks).delay();
-    // or
-    let mut delay = dp.TIM2.delay_us(&clocks);
+    // 具有自定义精度的阻塞延迟
+    let mut delay = cp.SYST.delay(&clocks);
 
     // 等待计时器触发更新并更改LED的状态
     loop {
         for i in 0..10 {
             println!("i={:?}", i);
             // Use `embedded_hal::DelayMs` trait
-            delay.delay_ms(1000_u32);
+            delay.delay_ms(1_000_u16);
         }
         // or use `fugit` duration units
-        delay.delay(3.secs());
+        delay.delay(1.secs());
     }
 }
